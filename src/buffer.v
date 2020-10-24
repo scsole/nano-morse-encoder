f7f2ddd008f44a3154aa6d971ff07b9e12c75b4b
Sebastian Sole <scsole@protonmail.com>
2020-10-24 16:31:04 +1300

    Switched to custom buffer for UART RX

---
+++ src/buffer.v
@@ -0,0 +1,80 @@
+///////////////////////////////////////////////////////////////////////////////
+// vim:set shiftwidth=4 softtabstop=4 expandtab:
+// Sebastian Sole, 2020
+//
+// Module: buffer.v
+// Project: Nano Morse Encoder
+// Description: Memory buffer for UART communication. Used as a FIFO data buffer
+//              Every i_rw HI signal saves data and outputs the location of the
+//              next address to be used. Data can be read from anywhere so
+//              i_rd_addr must be kept track of for proper FIFO function.
+//
+//
+// Change history:
+//      v01 - Initial buffer implementation
+//
+///////////////////////////////////////////////////////////////////////////////
+module buffer (
+    input i_clk_24,                 // 24 MHz system clk
+    input i_rst,                    // Synchronous reset, active HI
+    input i_rw,                     // Pulse HI to write i_data at o_rw_addr
+    input [10:0] i_rd_addr,         // Read address
+    input [7:0] i_data,              // Data to store when at o_rw_addr
+    output reg [10:0] o_wr_addr,    // The next address that will be written to
+    output [6:0] o_rd_data         // Data located at i_rd_addr
+);
+
+    wire [31:0] d_out;      // Data retrieved from memory
+    reg [31:0] d_in = 0;    // Data to store in memory
+    reg [31:0] d_buf = 0;   // Last retrieved data from memory
+    reg [10:0] d_addr = 0;  // Memory address of data to rd/rw
+    reg d_wre = 0;          // 1 to write to memory, else 0 to read from memory
+
+    assign o_rd_data = d_buf[6:0]; // Always allow data to be read
+
+    always @(posedge i_clk_24)
+    begin
+        if (i_rw) begin
+            // Write data to memory
+            d_addr <= o_wr_addr;
+            o_wr_addr <= o_wr_addr + 1;
+            d_in <= {24'b0, i_data};
+            d_wre <= 1;
+        end else begin
+            // Keep the read buffer up to date
+            d_addr <= i_rd_addr;
+            d_buf <= d_out;
+            d_wre <= 0;
+        end
+
+        if (i_rst) begin
+            o_wr_addr <= 11'b0;
+            d_in <= 31'b0;
+            d_buf <= 31'b0;
+            d_addr <= 11'b0;
+        end
+    end
+
+    // Memory configuration
+    wire gw_gnd;
+    assign gw_gnd = 1'b0;
+
+    SP sp_inst_0 (
+        .DO(d_out),
+        .CLK(i_clk_24),
+        .OCE(0),
+        .CE(1),
+        .RESET(i_rst),
+        .WRE(d_wre),
+        .BLKSEL({gw_gnd,gw_gnd,gw_gnd}),
+        .AD({d_addr,gw_gnd,gw_gnd,gw_gnd}),
+        .DI(d_in)
+    );
+
+    defparam sp_inst_0.READ_MODE = 1'b0;
+    defparam sp_inst_0.WRITE_MODE = 2'b00;
+    defparam sp_inst_0.BIT_WIDTH = 8;
+    defparam sp_inst_0.BLK_SEL = 3'b000;
+    defparam sp_inst_0.RESET_MODE = "SYNC";
+
+endmodule
--- src/top.v
+++ src/top.v
@@ -8,7 +8,9 @@
 //
 //
 // Change history:
-//      v01 -  Initial attempt to receive data using wbuart32 modules
+//      v01 - Initial attempt to receive data using wbuart32 modules
+//      v02 - Switch to own buffer module
+//      v03 - Transmit ASCII over serial when decoded for debugging
 //
 ///////////////////////////////////////////////////////////////////////////////
 `define ASCII_SPACE 8'd32
@@ -18,137 +20,144 @@
 `define DASH_DELAY 2'd3
 
 module top (
-    input clk_24,           // 24 MHz system clk
-    input rst,              // System reset
-    input uart_rx,          // UART recieve port
-    output uart_tx,         // UART transmit port, only used to transmit the
-                            // recieved value back to the connected device for 
-                            // debugging purposes
-    output [2:0] rgb_led,   // RGB led used to show signal state,
-    output signal           // Modulated output signal, produces 440 Hz tone
+    input clk_24,               // 24 MHz system clk
+    input rst_n,                // System reset
+    input uart_rx,              // UART recieve port
+    output uart_tx,             // UART transmit port, only used to transmit the
+                                // rx_ready value back to the connected device 
+                                // for debugging purposes
+    output [2:0] led,           // RGB LED used to show signal state
+    output signal,              // Modulated output signal, produces 440 Hz tone
+    output [10:0] mem_rw_addr   // The next memory address to write to. We must
+                                // stop reading if we reach this address as the
+                                // buffer will be empty.
 );
 
-    wire rx_ready;      // Data (rx_data) is available
-    wire [7:0] rx_data; // Recieved data
+    wire rx_ready;      // Data recieved successfully
+    wire [7:0] rx_data; // Actual data recieved
 
-    wire rxbuff_not_empty;  // HI if there is at least one item to be read, else LO
-    wire [7:0] rxbuff_data; // The next item to be read from the buffer
-    reg rxbuff_read = 0;    // Pulse HI to read an item from the buffer, and update rxbuff_data
+    reg tx_ready;       // Data is ready to be transmitted
+    reg [7:0] tx_data;  // Actual data to be transmitted
 
-    reg tx_ready;      // Data (tx_data) is ready to transmit
-    reg [7:0] tx_data; // Data to transmit
-    wire tx_busy;      // Data (tx_data) is being sent, must be LO before strobing tx_ready
+    reg [10:0] mem_rd_addr = 0; // The memory address to read from
+    wire [6:0] mem_rd_data;     // The 7-bit ASCII value read from memory
 
-    wire [6:0] morse_code;      // Morse code signals for the current ascii_code
-    wire [2:0] morse_len;       // The total number of signals in morse_code
-    reg  [2:0] morse_index = 0; // The current signal to generate in morse_code
+    wire [6:0] morse_code;     // Decoded Morse code signals for the current ASCII
+                               // code in mem_rd_data
+    wire [2:0] morse_len;      // The total number of signals in morse_code
+    reg [2:0] morse_index = 0; // The current Morse code signal to generate
 
-    reg signal_state = 0;     // The Morse code output state used to modulate signals
-    reg [1:0] delay_time = 0; // The number of units to wait before changing state
+    reg signal_state = 0;    // The Morse code output state used to modulate signals
+    reg[1:0] delay_time = 0; // The number of units to wait before changing state
 
     reg [23:0] counter = 0; // 24-bit counter, gives roughtly 700 ms @ 24 MHz
 
     localparam PERIOD = 50;                   // Duration of a dot in ms
     localparam CLK_DIVIDER = 24_000 * PERIOD; // 20 Hz clk for 50 ms period
 
+    
     always @(posedge clk_24) begin
-        if (rxbuff_read)
-            rxbuff_read <= 0;
-
         if (counter == CLK_DIVIDER) begin
             counter <= 0;
 
-            // Wait for a dot or dash to complete, or untill a new character is recieved
-            if (delay_time == 0 && rxbuff_not_empty) begin
+            // Wait for a dot or dash to complete, or untill the buffer has new data
+            if (delay_time == 0 && mem_rw_addr != mem_rd_addr) begin
 
                 if (morse_index == morse_len) begin
-                    // Move to the next caracter if the last signal was processed
+                    // The last signal was processed - move to the next character
                     signal_state <= 0;
                     delay_time <= `LETTER_SPACE;
                     morse_index <= 0;
-                    rxbuff_read <= 1; // Remove the first item from the FIFO buffer,
-                                      // we've finished transmitting it
+                    mem_rd_addr <= mem_rd_addr + 1; // 'Consume' the processed character
 
                 end else begin
-                    // Continue processing existing character
-                    if (signal_state) begin
-                        // Finished signal/pulse, move to the next
+                    // Still more signals - continue with the current character
+                    if (signal_state) begin // Finished signal/pulse - move to the next one
                         signal_state <= 0;
                         delay_time <= `SYMBOL_SPACE;
-                    end else begin
-                        // Begin a signal/pulse
+
+                    end else begin // Begin a new signal/pulse
                         delay_time <= morse_code[morse_index] ? `DASH_DELAY : `DOT_DELAY;
                         morse_index <= morse_index + 1;
-                        signal_state <= (rxbuff_data != `ASCII_SPACE) ? 1 : 0; // Space is held LO
+                        signal_state <= (mem_rd_data == `ASCII_SPACE) ? 0 : 1;
+
+                        // Transmit the processing character over serial once
+                        if (morse_index == 0) begin
+                            tx_data <= mem_rd_data;
+                            tx_ready <= 1;
+                        end
                     end
                 end
             end else begin
                 delay_time <= delay_time - 1;
             end
-        end else
-            counter <= counter + 1;
 
-        if (~rst) begin
+        end else begin
+            counter <= counter + 1;
+            tx_ready <= 0; // Reset tx_ready (as the caracter would have started or finished transmitting)
+        end
+
+        if (~rst_n) begin
             counter <= 0;
             delay_time <= 0;
             signal_state <= 0;
             morse_index <= 0;
-            tx_ready <= 0;
+            mem_rd_addr <= 0;
             tx_data <= 0;
-            rxbuff_read <= 0;
+            tx_ready <= 8'b0;
         end
     end
 
-    // Defines 8-bit word, one stop bit, no parity and 9600 baud over a 24 MHz clock
-    reg [30:0] setup = {7'b100000,24'd2500}; // Setup register for UART modules
 
+    // Setup register for UART modules
+    // 8-bit word, one stop bit, no parity and 9600 baud over a 24 MHz clock
+    reg [30:0] setup = {7'b1000000, 24'd2500};
+
+    // Connect UART modules withouth worying about error handling, simple but not roboust
     rxuart rx0(
         .i_clk(clk_24),
-        .i_reset(~rst),
+        .i_reset(~rst_n),
         .i_setup(setup),
         .i_uart_rx(uart_rx),
         .o_wr(rx_ready),
-        .o_data(rx_data),
-        .o_break(),
-		.o_parity_err(),
-        .o_frame_err(),
-        .o_ck_uart()
+        .o_data(rx_data)
     );
 
     txuart tx0(
         .i_clk(clk_24),
-        .i_reset(~rst),
+        .i_reset(~rst_n),
         .i_setup(setup),
         .i_break(1'b0),
         .i_wr(tx_ready),
         .i_data(tx_data),
-		.i_cts_n(1'b0),
-        .o_uart_tx(uart_tx),
-        .o_busy(tx_busy)
+        .i_cts_n(1'b0),
+        .o_uart_tx(uart_tx)
     );
 
-    // Synchronous data FIFO for rx0
-    ufifo rx0_fifo(
-        .i_clk(clk_24),
-        .i_reset(~rst),
-        .i_wr(rx_ready),
+    // Buffer for RX UART module. We can recieve characters much faster than we
+    // transmit them as Morse code. Hence we need a FIFO data buffer. Since we
+    // transmit far slower than the UART module can handle, we can use it
+    // without one.
+    buffer rx_buff0(
+        .i_clk_24(clk_24),
+        .i_rst(~rst_n),
+        .i_rw(rx_ready),
+        .i_rd_addr(mem_rd_addr),
         .i_data(rx_data),
-        .o_empty_n(rxbuff_not_empty),
-        .i_rd(rxbuff_read),
-        .o_data(rxbuff_data),
-        .o_status(),
-        .o_err()
+        .o_wr_addr(mem_rw_addr),
+        .o_rd_data(mem_rd_data)
     );
 
     // Use a (white) LED to show the signal output
     // LEDs are active low so require an inverted signal
-    assign rgb_led = {~signal_state, ~signal_state, ~signal_state};
+    assign led = {~signal_state, ~signal_state, ~signal_state};
+
 
     // Decode an ASCII code into Morse code signals
     ascii_2_morse m0(
         .clk_24(clk_24),
-        .rst(~sys_rst),
-        .ascii_code(rxbuff_data[6:0]),
+        .rst(~rst_n),
+        .ascii_code(mem_rd_data),
         .morse_code(morse_code),
         .morse_len(morse_len)
     );
@@ -156,8 +165,9 @@ module top (
     // Drive the 440 Hz tone output
     morse_key s0(
         .clk_24(clk_24),
-        .rst(rst),
+        .rst(rst_n),
         .key(signal_state),
         .signal(signal)
     );
+
 endmodule
